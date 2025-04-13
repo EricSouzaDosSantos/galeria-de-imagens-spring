ce57a87588dc1c5ae3190e2938770f8a5a86ac93
Eric <ericsouza94895@gmail.com>
2025-04-12 23:19:02 -0300

    first commit

---
+++ src/main/java/com/desafios/galeriaimagensspring/GaleriaImagensSpringApplication.java
@@ -0,0 +1,13 @@
+package com.desafios.galeriaimagensspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaleriaImagensSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaleriaImagensSpringApplication.class, args);
+    }
+
+}
