eaec5bd9cd6ecfa0a96ad530a3ae1513121bdec5
Eric <ericsouza94895@gmail.com>
2025-04-13 22:58:19 -0300

    feat: add user entity

---
+++ src/main/java/com/desafios/galeriaimagensspring/domain/model/User.java
@@ -0,0 +1,24 @@
+package com.desafios.galeriaimagensspring.domain.model;
+
+import com.desafios.galeriaimagensspring.domain.model.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+}
