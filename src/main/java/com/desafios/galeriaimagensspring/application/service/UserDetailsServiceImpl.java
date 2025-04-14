ee141c77b33371ebcaf532911631f60188a266dc
Eric <ericsouza94895@gmail.com>
2025-04-13 23:01:36 -0300

    feat: add service to authenticate the user and return the user details

---
+++ src/main/java/com/desafios/galeriaimagensspring/application/service/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.desafios.galeriaimagensspring.application.service;
+
+import com.desafios.galeriaimagensspring.domain.model.User;
+import com.desafios.galeriaimagensspring.domain.model.UserDetailsImpl;
+import com.desafios.galeriaimagensspring.domain.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow( () -> new UsernameNotFoundException("User not found with email: " + username));
+        return new UserDetailsImpl(user);
+    }
+}
