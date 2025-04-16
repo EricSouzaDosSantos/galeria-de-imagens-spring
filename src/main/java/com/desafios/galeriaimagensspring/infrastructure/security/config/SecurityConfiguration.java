11062d31791146c8be6b31658df92ee58d2c3426
Eric <ericsouza94895@gmail.com>
2025-04-15 21:43:26 -0300

    feat: add the class that enables the api security configuration

---
+++ src/main/java/com/desafios/galeriaimagensspring/infrastructure/security/config/SecurityConfiguration.java
@@ -0,0 +1,65 @@
+package com.desafios.galeriaimagensspring.infrastructure.security.config;
+
+import com.desafios.galeriaimagensspring.infrastructure.security.filters.UserAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    public static final String[] ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED = {
+            "/auth/login",
+            "/auth/register",
+            "/auth/refresh-token",
+            "/auth/forgot-password",
+            "/auth/reset-password",
+            "/auth/verify-email",
+            "/auth/verify-email/{token}",
+    };
+
+    public static final String[] ENDPOINTS_WITH_AUTHENTICATION_REQUIRED = {
+            ""
+    };
+
+    public static final String[] ENDPOINTS_VIEWER = {
+            ""
+    };
+
+    public static final String[] ENDPOINTS_USER = {
+            ""
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, UserAuthenticationFilter userAuthenticationFilter) throws Exception{
+
+        httpSecurity.csrf( csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers(ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(userAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+ }
