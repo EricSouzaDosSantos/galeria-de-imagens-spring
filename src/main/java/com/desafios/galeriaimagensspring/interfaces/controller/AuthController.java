051f79d1e97532e1b0ee31a52fd1862c728a097f
Eric <ericsouza94895@gmail.com>
2025-04-16 23:55:57 -0300

    feat: add class authController

---
+++ src/main/java/com/desafios/galeriaimagensspring/interfaces/controller/AuthController.java
@@ -0,0 +1,34 @@
+package com.desafios.galeriaimagensspring.interfaces.controller;
+
+import com.desafios.galeriaimagensspring.application.service.UserService;
+import com.desafios.galeriaimagensspring.infrastructure.security.dtos.LoginUserDTO;
+import com.desafios.galeriaimagensspring.infrastructure.security.dtos.RecoveryJwtTokenDTO;
+import com.desafios.galeriaimagensspring.infrastructure.security.dtos.RegisterUserDTO;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<RecoveryJwtTokenDTO> login(@RequestBody LoginUserDTO loginUserDTO){
+        RecoveryJwtTokenDTO token = userService.authenticateUser(loginUserDTO);
+        return ResponseEntity.ok().body(token);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterUserDTO registerUserDTO){
+        userService.registerUser(registerUserDTO);
+        return ResponseEntity.status(201).body("Usuário criado com sucesso");
+    }
+}
