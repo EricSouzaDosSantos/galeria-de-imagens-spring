10942263dfba319d43bde3dd19efc93c08388e65
Eric <ericsouza94895@gmail.com>
2025-04-20 23:39:28 -0300

    style: removing unnecessary imports and changing the service import

--- src/main/java/com/desafios/galeriaimagensspring/interfaces/controller/AuthController.java
+++ src/main/java/com/desafios/galeriaimagensspring/interfaces/controller/AuthController.java
@@ -1,14 +1,12 @@
 package com.desafios.galeriaimagensspring.interfaces.controller;
 
-import com.desafios.galeriaimagensspring.application.service.UserService;
+import com.desafios.galeriaimagensspring.domain.service.UserService;
 import com.desafios.galeriaimagensspring.infrastructure.security.dtos.LoginUserDTO;
 import com.desafios.galeriaimagensspring.infrastructure.security.dtos.RecoveryJwtTokenDTO;
 import com.desafios.galeriaimagensspring.infrastructure.security.dtos.RegisterUserDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
