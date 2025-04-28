a5d0eab427d5d65978cb10f387f457a49b98d784
Eric <ericsouza94895@gmail.com>
2025-04-28 01:12:12 -0300

    feat: add exception for image not found

---
+++ src/main/java/com/desafios/galeriaimagensspring/domain/exception/image/ImageNotFoundException.java
@@ -0,0 +1,7 @@
+package com.desafios.galeriaimagensspring.domain.exception.image;
+
+public class ImageNotFoundException extends RuntimeException {
+    public ImageNotFoundException(String message) {
+        super(message);
+    }
+}
