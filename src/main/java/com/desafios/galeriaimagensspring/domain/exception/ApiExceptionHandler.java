5610d4f25180e9263d5e9cebd786d8179d7e8161
Eric <ericsouza94895@gmail.com>
2025-04-28 01:13:09 -0300

    feat: add exception handler to handle all application exceptions

---
+++ src/main/java/com/desafios/galeriaimagensspring/domain/exception/ApiExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.desafios.galeriaimagensspring.domain.exception;
+
+import com.desafios.galeriaimagensspring.domain.exception.image.ImageNotFoundException;
+import com.desafios.galeriaimagensspring.domain.exception.user.UnauthorizedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(ImageNotFoundException.class)
+    public ResponseEntity<String> handleImageNotFoundException(ImageNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<String> handleUnauthorizedException(UnauthorizedException ex) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("An unexpected server error occurred: " + ex.getMessage());
+    }
+}
