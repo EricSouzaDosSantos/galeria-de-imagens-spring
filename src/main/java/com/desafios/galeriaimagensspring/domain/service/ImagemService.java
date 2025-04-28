485b5bc9d64355ba9c76fd821258f35674c56edf
Eric <ericsouza94895@gmail.com>
2025-04-27 23:54:19 -0300

    feat: add service to execute the use cases of creating, updating, and deleting images

--- src/main/java/com/desafios/galeriaimagensspring/domain/service/ImagemService.java
+++ src/main/java/com/desafios/galeriaimagensspring/domain/service/ImagemService.java
@@ -1,30 +1,69 @@
 package com.desafios.galeriaimagensspring.domain.service;
 
-import com.desafios.galeriaimagensspring.domain.model.Album;
+import com.desafios.galeriaimagensspring.application.dto.GetImageDto;
+import com.desafios.galeriaimagensspring.application.dto.SaveImageDTO;
 import com.desafios.galeriaimagensspring.domain.model.Imagens;
-import com.desafios.galeriaimagensspring.domain.repository.AlbumRepository;
 import com.desafios.galeriaimagensspring.domain.repository.ImagensRepository;
-import lombok.AllArgsConstructor;
+import com.desafios.galeriaimagensspring.usecase.image.delete.DeleteImageUseCase;
+import com.desafios.galeriaimagensspring.usecase.image.save.SaveImageUseCase;
+import com.desafios.galeriaimagensspring.usecase.image.update.UpdateImageUseCase;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class ImagemService {
 
     private final ImagensRepository imagensRepository;
-    private final AlbumRepository albumRepository;
+    private final SaveImageUseCase saveImageUseCase;
+    private final DeleteImageUseCase deleteImageUseCase;
+    private final UpdateImageUseCase updateImageUseCase;
 
-    public Imagens execute(MultipartFile file, Long albumId) {
-        Album album = albumRepository.findById(albumId)
-                .orElseThrow(() -> new RuntimeException("Álbum não encontrado"));
-
+    public Imagens saveImage(SaveImageDTO saveImageDTO, MultipartFile file) {
+        String imageURL = saveImageUseCase.execute(file);
         Imagens imagem = new Imagens();
-        imagem.setNameImage(file.getOriginalFilename());
-        imagem.setImageURL("");
-        imagem.setAlbum(album);
+        imagem.setName(file.getOriginalFilename());
+        imagem.setDescription(saveImageDTO.description());
+        imagem.setImageURL(imageURL);
+        imagem.setAlternateText(saveImageDTO.alternateText());
+        imagem.setAlbum(saveImageDTO.albums());
 
         return imagensRepository.save(imagem);
+    }
+
+    public void deleteImage(String imageURL) {
+        deleteImageUseCase.execute(imageURL);
+    }
+
+    public Imagens updateImage(String oldImageUrl, MultipartFile file){
+        String newImageUrl = updateImageUseCase.execute(oldImageUrl, file);
+        Imagens imagem = imagensRepository.findByImageURL(oldImageUrl);
+        imagem.setImageURL(newImageUrl);
+        imagem.setName(file.getOriginalFilename());
+        return imagensRepository.save(imagem);
+    }
+
+    public List<GetImageDto> findAllImages() {
+        return imagensRepository.findAll().stream().map(imagem -> {
+            GetImageDto getImageDto = new GetImageDto(imagem.getName(),
+                    imagem.getDescription(),
+                    imagem.getCreationDate(),
+                    imagem.getAlternateText(),
+                    imagem.getImageURL());
+            return getImageDto;
+        }).toList();
+    }
+
+    public GetImageDto findImageById(Long id) {
+        Imagens imagem = imagensRepository.findById(id).orElseThrow(() -> new RuntimeException("Image not found"));
+        return new GetImageDto(imagem.getName(),
+                imagem.getDescription(),
+                imagem.getCreationDate(),
+                imagem.getAlternateText(),
+                imagem.getImageURL());
     }
 
 
