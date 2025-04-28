a275f11b283ed7a223c13630a9ef15323152617e
Eric <ericsouza94895@gmail.com>
2025-04-27 23:47:37 -0300

    refactor: add query to search for images by url

--- src/main/java/com/desafios/galeriaimagensspring/domain/repository/ImagensRepository.java
+++ src/main/java/com/desafios/galeriaimagensspring/domain/repository/ImagensRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ImagensRepository extends JpaRepository<Imagens, Long> {
+    Imagens findByImageURL(String imageURL);
 }
