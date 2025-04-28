2f41a33026d5bd4524310a2ab1e15d4d0f172676
Eric <ericsouza94895@gmail.com>
2025-04-28 01:14:50 -0300

    refactor: refactoring the controller's post method to accept multipartFiles

--- src/main/java/com/desafios/galeriaimagensspring/interfaces/controller/ImageController.java
+++ src/main/java/com/desafios/galeriaimagensspring/interfaces/controller/ImageController.java
@@ -2,6 +2,7 @@ package com.desafios.galeriaimagensspring.interfaces.controller;
 
 import com.desafios.galeriaimagensspring.application.dto.GetImageDto;
 import com.desafios.galeriaimagensspring.application.dto.SaveImageDTO;
+import com.desafios.galeriaimagensspring.domain.model.Album;
 import com.desafios.galeriaimagensspring.domain.model.Imagens;
 import com.desafios.galeriaimagensspring.domain.service.ImagemService;
 import lombok.RequiredArgsConstructor;
@@ -18,11 +19,19 @@ public class ImageController {
 
     private final ImagemService imagemService;
 
-    @PostMapping
-    public ResponseEntity<Imagens> saveImage(@RequestPart SaveImageDTO saveImageDTO, @RequestPart MultipartFile image) {
-        Imagens savedImage = imagemService.saveImage(saveImageDTO, image);
+    @PostMapping(consumes = "multipart/form-data")
+    public ResponseEntity<Imagens> saveImage(
+            @RequestPart("image") MultipartFile image,
+            @RequestPart("alternateText") String alternateText,
+            @RequestPart(value = "name", required = false) String name,
+            @RequestPart(value = "description", required = false) String description,
+            @RequestPart(value = "albums", required = false) List<Album> albums) {
+
+        SaveImageDTO saveImageDTO = new SaveImageDTO(name, description, alternateText, image, albums);
+        Imagens savedImage = imagemService.saveImage(saveImageDTO);
         return ResponseEntity.status(201).body(savedImage);
     }
+
 
     @GetMapping
     public ResponseEntity<List<GetImageDto>> getAllImages() {
