38e4356a0d2046fe75ab111c25ea288c029b50eb
Eric <ericsouza94895@gmail.com>
2025-04-27 23:54:32 -0300

    feat: add controller for images

---
+++ src/main/java/com/desafios/galeriaimagensspring/interfaces/controller/ImageController.java
@@ -0,0 +1,44 @@
+package com.desafios.galeriaimagensspring.interfaces.controller;
+
+import com.desafios.galeriaimagensspring.application.dto.GetImageDto;
+import com.desafios.galeriaimagensspring.application.dto.SaveImageDTO;
+import com.desafios.galeriaimagensspring.domain.model.Imagens;
+import com.desafios.galeriaimagensspring.domain.service.ImagemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/images")
+@RequiredArgsConstructor
+public class ImageController {
+
+    private final ImagemService imagemService;
+
+    @PostMapping
+    public ResponseEntity<Imagens> saveImage(@RequestPart SaveImageDTO saveImageDTO, @RequestPart MultipartFile image) {
+        Imagens savedImage = imagemService.saveImage(saveImageDTO, image);
+        return ResponseEntity.status(201).body(savedImage);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<GetImageDto>> getAllImages() {
+        List<GetImageDto> images = imagemService.findAllImages();
+        return ResponseEntity.ok(images);
+    }
+
+    @PutMapping("/{oldImageUrl}")
+    public ResponseEntity<Imagens> updateImage(@PathVariable String oldImageUrl, @RequestPart MultipartFile image) {
+        Imagens updatedImage = imagemService.updateImage(oldImageUrl, image);
+        return ResponseEntity.ok(updatedImage);
+    }
+
+    @DeleteMapping("/{imageURL}")
+    public ResponseEntity<Void> deleteImage(@PathVariable String imageURL) {
+        imagemService.deleteImage(imageURL);
+        return ResponseEntity.noContent().build();
+    }
+}
