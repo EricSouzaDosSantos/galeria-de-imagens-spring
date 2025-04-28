494db5adc5bf774f8014614a2caced388f7abf69
Eric <ericsouza94895@gmail.com>
2025-04-28 01:07:50 -0300

    refactor: changing the return of the find by imageUrl method into optional to handle a possible nullPointerException

--- src/main/java/com/desafios/galeriaimagensspring/domain/repository/ImagensRepository.java
+++ src/main/java/com/desafios/galeriaimagensspring/domain/repository/ImagensRepository.java
@@ -4,7 +4,9 @@ import com.desafios.galeriaimagensspring.domain.model.Imagens;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ImagensRepository extends JpaRepository<Imagens, Long> {
-    Imagens findByImageURL(String imageURL);
+    Optional<Imagens> findByImageURL(String imageURL);
 }
