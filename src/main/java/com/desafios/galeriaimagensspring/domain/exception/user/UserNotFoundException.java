031288752e973af043740303aacc1f971484ee8e
Eric <ericsouza94895@gmail.com>
2025-04-29 23:48:55 -0300

    feat: add exception for when a user is not found

---
+++ src/main/java/com/desafios/galeriaimagensspring/domain/exception/user/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.desafios.galeriaimagensspring.domain.exception.user;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
