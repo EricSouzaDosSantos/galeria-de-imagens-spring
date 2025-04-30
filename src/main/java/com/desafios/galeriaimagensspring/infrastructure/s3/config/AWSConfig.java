76c4d2d691f2664853af04cec2f6e193b0215cd7
Eric <ericsouza94895@gmail.com>
2025-04-29 23:48:13 -0300

    refactor: changing the class to s3Cliente

--- src/main/java/com/desafios/galeriaimagensspring/infrastructure/s3/config/AWSConfig.java
+++ src/main/java/com/desafios/galeriaimagensspring/infrastructure/s3/config/AWSConfig.java
@@ -1,11 +1,14 @@
 package com.desafios.galeriaimagensspring.infrastructure.s3.config;
 
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProviderChain;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3ClientBuilder;
 
 @Configuration
 public class AWSConfig {
@@ -14,13 +17,10 @@ public class AWSConfig {
     private String region;
 
     @Bean
-    public AmazonS3 amazonS3(){
-        AmazonS3 s3Client = AmazonS3ClientBuilder
-                .standard()
-                .withCredentials(new DefaultAWSCredentialsProviderChain())
-                .withRegion(region)
+    public S3Client amazonS3(){
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
-
-        return s3Client;
     }
 }
