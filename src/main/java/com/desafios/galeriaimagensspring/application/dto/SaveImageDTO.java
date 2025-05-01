2103af67a1777eb7c7bda74253ba3e07e8226737
Eric <ericsouza94895@gmail.com>
2025-04-30 21:44:17 -0300

    feat: add dto to save data of image entity

--- src/main/java/com/desafios/galeriaimagensspring/application/dto/SaveImageDTO.java
+++ src/main/java/com/desafios/galeriaimagensspring/application/dto/SaveImageDTO.java
@@ -4,5 +4,5 @@ import com.desafios.galeriaimagensspring.domain.model.Album;
 import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
-public record SaveImageDTO(String name, String description, String alternateText, MultipartFile image, List<Album> albums) {
+public record SaveImageDTO(String name, String description, String alternateText, MultipartFile image) {
 }
