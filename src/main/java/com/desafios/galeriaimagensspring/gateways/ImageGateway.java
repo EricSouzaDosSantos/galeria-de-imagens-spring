5e206a16856ee48d329173976a99407858f8fc95
Eric <ericsouza94895@gmail.com>
2025-06-29 23:48:47 -0300

    feat: implementing method to upload images to image gateway

--- src/main/java/com/desafios/galeriaimagensspring/gateways/ImageGateway.java
+++ src/main/java/com/desafios/galeriaimagensspring/gateways/ImageGateway.java
@@ -1,10 +1,12 @@
 package com.desafios.galeriaimagensspring.gateways;
 
 import com.desafios.galeriaimagensspring.core.model.Imagem;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface ImageGateway {
     Imagem save(Imagem image);
     Imagem findById(Long id);
     void deleteById(Long id);
+    String uploadImage(MultipartFile image, String folderName);
 
 }
