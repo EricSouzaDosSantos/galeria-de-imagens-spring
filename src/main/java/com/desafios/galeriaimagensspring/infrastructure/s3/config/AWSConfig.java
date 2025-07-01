50a7c1155f2559db2d12d4cade25f7ef53f5a6fd
Eric <ericsouza94895@gmail.com>
2025-06-30 23:54:02 -0300

    style: removing imports that are not being used

--- src/main/java/com/desafios/galeriaimagensspring/infrastructure/s3/config/AWSConfig.java
+++ src/main/java/com/desafios/galeriaimagensspring/infrastructure/s3/config/AWSConfig.java
@@ -4,11 +4,9 @@ package com.desafios.galeriaimagensspring.infrastructure.s3.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProviderChain;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.S3ClientBuilder;
 
 @Configuration
 public class AWSConfig {
