1f828eb8550912634bada2f3b9be1093b1dc8c9a
Eric <ericsouza94895@gmail.com>
2025-07-09 23:00:38 -0300

    bugfix: fixing error in writing the query to update the image table in the database

--- src/main/java/com/desafios/galeriaimagensspring/infrastructure/persistence/repository/image/SpringDataImageRepository.java
+++ src/main/java/com/desafios/galeriaimagensspring/infrastructure/persistence/repository/image/SpringDataImageRepository.java
@@ -1,14 +1,20 @@
 package com.desafios.galeriaimagensspring.infrastructure.persistence.repository.image;
 
 import com.desafios.galeriaimagensspring.infrastructure.persistence.entity.ImagemEntity;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
 public interface SpringDataImageRepository extends JpaRepository<ImagemEntity, Long> {
     Optional<ImagemEntity> findByImageURL(String url);
     void deleteByImageURL(String url);
-    void updateImageURLByImageURL(String newUrl, String oldUrl);
+    @Modifying
+    @Transactional
+    @Query("UPDATE ImagemEntity i SET i.imageURL = :newUrl WHERE i.imageURL = :oldUrl")
+    void updateImageURLByImageURL(@Param("newUrl") String newUrl, @Param("oldUrl") String oldUrl);
 }
