df1c72af8c942e0dd47715ac960b7d4a36aa342b
Eric <ericsouza94895@gmail.com>
2025-07-14 16:30:20 -0300

    feat: add SwaggerConfig

---
+++ src/main/java/com/desafios/galeriaimagensspring/infrastructure/configuration/documentation/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package com.desafios.galeriaimagensspring.infrastructure.configuration.documentation;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    String schemeName = "bearerAuth";
+    String bearerFormat = "JWT";
+    String scheme = "bearer";
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+    @Bean
+    public OpenAPI caseOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(schemeName)).components(new Components()
+                        .addSecuritySchemes(
+                                schemeName, new SecurityScheme()
+                                        .name(schemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .bearerFormat(bearerFormat)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .scheme(scheme)
+                        )
+                )
+                .info(new Info()
+                        .title("Galeria de Imagens API Documentation")
+                        .description("This API provides endpoints for managing images and albums in a gallery application.")
+                        .version("1.0")
+                );
+    }
+}
