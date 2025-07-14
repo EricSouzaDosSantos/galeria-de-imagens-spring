d2c6ce1e2551adc3365e32e5db09475195bc8ff7
Eric <ericsouza94895@gmail.com>
2025-07-13 23:47:04 -0300

    fix: add repository annotation for springDataRepository

--- src/main/java/com/desafios/galeriaimagensspring/infrastructure/persistence/repository/album/SpringDataAlbumRepository.java
+++ src/main/java/com/desafios/galeriaimagensspring/infrastructure/persistence/repository/album/SpringDataAlbumRepository.java
@@ -2,7 +2,9 @@ package com.desafios.galeriaimagensspring.infrastructure.persistence.repository.
 
 import com.desafios.galeriaimagensspring.infrastructure.persistence.entity.AlbumEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SpringDataAlbumRepository extends JpaRepository<AlbumEntity, Long> {
 
 }
