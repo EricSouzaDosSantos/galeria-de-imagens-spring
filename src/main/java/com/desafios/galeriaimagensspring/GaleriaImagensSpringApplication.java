1cf356a31842c6197d992672888f15a5c4bbe970
Eric <ericsouza94895@gmail.com>
2025-07-14 17:12:12 -0300

    feat: add configuration to load data from .env file

--- src/main/java/com/desafios/galeriaimagensspring/GaleriaImagensSpringApplication.java
+++ src/main/java/com/desafios/galeriaimagensspring/GaleriaImagensSpringApplication.java
@@ -1,5 +1,6 @@
 package com.desafios.galeriaimagensspring;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GaleriaImagensSpringApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
         SpringApplication.run(GaleriaImagensSpringApplication.class, args);
     }
 
